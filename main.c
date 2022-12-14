d25f1a4da1b980e0f1ca277aa6e0d83291d2922e
yahdielo <yahdielaz@gmail.com>
2022-12-14 19:51:05 +0000

    main.c

--- main.c
+++ main.c
@@ -1,16 +1,23 @@
 #include "monty.h"
+/**
+ * main - read command arguments, opens file
+ *
+ */
 
 int main(int ac, char **av)
 {
 	FILE *file;
 	char *line = NULL;
 	size_t buffline = 0;
-	
+	stack_t *stack = NULL;
+	unsigned int line_number = 0;
 
 	/* checking number of argument */
 	if (ac != 2)
+	{
+		fprintf(stderr, "USAGE: monty file");
 		exit(EXIT_FAILURE);
-
+	}
 	/* opening file */
 	file = fopen(av[1], "r");
 
@@ -18,8 +25,17 @@ int main(int ac, char **av)
 	while (getline(&line, &buffline, file) != -1)
 	{
 		/* TOKENIZATION */
+		
+		tokens = tokenization(line, " \n");
 
-		printf("%s\n", line);
+		if (strcmp(tokens[0], "push") == 0)
+		{
+			push(&stack, line_number);
+		}
+		if (strcmp(tokens[0], "pall") == 0)
+		{
+			pall(&stack, line_number);
+		}
 	}
 
 	/* closing file */
