d0a15e26e175409492bec3de44d337f4dc0cf223
yahdielo <yahdielaz@gmail.com>
2022-12-18 06:04:15 +0000

    added swap

--- monty.h
+++ monty.h
@@ -50,5 +50,6 @@ void pall(stack_t **stack, unsigned int line_numberar, char  *line, FILE *file);
 void add(stack_t **stack, unsigned int line_number, char *line, FILE *file);
 void pop(stack_t **stack, unsigned int line_number, char *line, FILE *file);
 void nop(stack_t **stack, unsigned int line_number, char *line, FILE *file);
+void swap(stack_t **stack, unsigned int line_number, char *line, FILE *file);
 void unkn_func(stack_t **stack, unsigned int line_number, char *line, FILE *file);
 #endif
